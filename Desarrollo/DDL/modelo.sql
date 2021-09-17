943ca76cd85cc0c8e0fc3c27c0b85516f3f1f861
Walter Arce <walterarce@gmail.com>
2021-09-17 20:44:31 -0300

    adjunto nuevo modelo de datos, con modificaciones revisadas

--- Desarrollo/DDL/modelo.sql
+++ Desarrollo/DDL/modelo.sql
@@ -1,18 +1,18 @@
-create table tp_base_datos_horarios.centro_costos
+create table centro_costos
 (
 	id_CC int auto_increment
 		primary key,
 	CC_Descripcion varchar(255) null
 );
 
-create table tp_base_datos_horarios.centro_facturacion
+create table centro_facturacion
 (
 	id_CF int auto_increment
 		primary key,
 	CF_Descripcion varchar(255) null
 );
 
-create table tp_base_datos_horarios.cliente
+create table cliente
 (
 	id int auto_increment
 		primary key,
@@ -21,12 +21,12 @@ create table tp_base_datos_horarios.cliente
 	id_cc int null,
 	id_cf int null,
 	constraint cliente_centro_costos_id_CC_fk
-		foreign key (id_cc) references tp_base_datos_horarios.centro_costos (id_CC),
+		foreign key (id_cc) references centro_costos (id_CC),
 	constraint cliente_centro_facturacion_id_CF_fk
-		foreign key (id_cf) references tp_base_datos_horarios.centro_facturacion (id_CF)
+		foreign key (id_cf) references centro_facturacion (id_CF)
 );
 
-create table tp_base_datos_horarios.empleado
+create table empleado
 (
 	id int,
 	nombre varchar(200) null,
@@ -37,23 +37,25 @@ create table tp_base_datos_horarios.empleado
 );
 
 create index empleado_id_legajo_index
-	on tp_base_datos_horarios.empleado (id, legajo);
+	on empleado (id, legajo);
 
-alter table tp_base_datos_horarios.empleado modify id int auto_increment;
+alter table empleado modify id int auto_increment;
 
-create table tp_base_datos_horarios.liquidacion_mensual
+create table liquidacion_mensual
 (
 	id int auto_increment
 		primary key,
 	cliente_id int null,
 	proyecto_id int null,
-	descripcion varchar(255) null
+	descripcion varchar(255) null,
+	horas decimal null,
+	fecha datetime null
 );
 
 create index liquidacion_mensual_clientes_id_fk
-	on tp_base_datos_horarios.liquidacion_mensual (cliente_id);
+	on liquidacion_mensual (cliente_id);
 
-create table tp_base_datos_horarios.proyecto
+create table proyecto
 (
 	proyecto_id int auto_increment
 		primary key,
@@ -61,43 +63,45 @@ create table tp_base_datos_horarios.proyecto
 	cliente_id int null,
 	estado char default 'A' null,
 	constraint proyecto_cliente_id_fk
-		foreign key (cliente_id) references tp_base_datos_horarios.cliente (id)
+		foreign key (cliente_id) references cliente (id)
 );
 
-create table tp_base_datos_horarios.roles
+create table roles
 (
 	id int auto_increment
 		primary key,
 	descripcion_rol varchar(255) null
 );
 
-create table tp_base_datos_horarios.roles_empleados
+create table roles_empleados
 (
-	id_rol int not null,
-	legajo_empleado int not null,
-	primary key (id_rol, legajo_empleado),
+	id_rol int null,
+	legajo_empleado int null,
+	constraint roles_empleados_pk
+		unique (id_rol, legajo_empleado),
 	constraint roles_empleados_empleado_legajo_fk
-		foreign key (legajo_empleado) references tp_base_datos_horarios.empleado (legajo),
+		foreign key (legajo_empleado) references empleado (legajo),
 	constraint roles_empleados_roles_id_fk
-		foreign key (id_rol) references tp_base_datos_horarios.roles (id)
+		foreign key (id_rol) references roles (id)
 );
 
-create table tp_base_datos_horarios.tareas
+create table tareas
 (
 	id int auto_increment
 		primary key,
 	descripcion varchar(255) null,
-	id_rendicion int null,
 	legajo_id int null,
 	estado char default 'A' null,
 	proyecto_id int null,
+	fini date null,
+	ffin datetime null,
 	constraint tareas_empleado_legajo_fk
-		foreign key (legajo_id) references tp_base_datos_horarios.empleado (legajo),
+		foreign key (legajo_id) references empleado (legajo),
 	constraint tareas_proyecto_proyecto_id_fk
-		foreign key (proyecto_id) references tp_base_datos_horarios.proyecto (proyecto_id)
+		foreign key (proyecto_id) references proyecto (proyecto_id)
 );
 
-create table tp_base_datos_horarios.rendicion
+create table rendicion
 (
 	id int auto_increment
 		primary key,
@@ -105,6 +109,19 @@ create table tp_base_datos_horarios.rendicion
 	tipo_rendicion char null,
 	tarea_id int null,
 	constraint rendicion_tareas_id_fk
-		foreign key (tarea_id) references tp_base_datos_horarios.tareas (id)
+		foreign key (tarea_id) references tareas (id)
 );
+
+create table tarea_rendicion
+(
+	id_tarea int null,
+	id_rendicion int null,
+	constraint tarea_rendicion_rendicion_id_fk
+		foreign key (id_rendicion) references rendicion (id),
+	constraint tarea_rendicion_tareas_id_fk
+		foreign key (id_tarea) references tareas (id)
+);
+
+create index tarea_rendicion_id_tarea_id_rendicion_index
+	on tarea_rendicion (id_tarea, id_rendicion);
 
